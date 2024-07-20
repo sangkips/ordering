2ea2629e7893486d0d7fe0a2cbb19938b96fbdb6
sangkips <sangkipkoech@gmail.com>
2024-07-20 15:44:23 +0300

    fix: add environment secrets

--- apps/orders/sms.py
+++ apps/orders/sms.py
@@ -4,14 +4,14 @@ from decouple import config
 url = "https://api.sandbox.africastalking.com/version1/messaging"
 
 headers = {
-    "ApiKey": config("AFRICASTALKING_API_KEY", default='mydefaultvalue'),
+    "ApiKey": config("AFRICASTALKING_API_KEY"),
     "Content-Type": "application/x-www-form-urlencoded",
     "Accept": "application/json",
 }
 
 data = {
-    "username": config("AFRICASTALKING_USERNAME", default='sandbox'),
-    "from": config("SMS_SENDER", default='1111'),
+    "username": config("AFRICASTALKING_USERNAME"),
+    "from": config("SMS_SENDER"),
     "message": "Thank you for your order. We will reach out to you shortly.",
     "to": "+254729640480",
 }
