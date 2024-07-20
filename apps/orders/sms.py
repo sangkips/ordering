eb5255f28f3e49c7e68b873382b20796995b35de
sangkips <sangkipkoech@gmail.com>
2024-07-20 15:16:19 +0300

    fix: fix config issue

--- apps/orders/sms.py
+++ apps/orders/sms.py
@@ -4,13 +4,13 @@ from decouple import config
 url = "https://api.sandbox.africastalking.com/version1/messaging"
 
 headers = {
-    "ApiKey": config("AFRICASTALKING_API_KEY"),
+    "ApiKey": config("AFRICASTALKING_API_KEY", default='mydefaultvalue'),
     "Content-Type": "application/x-www-form-urlencoded",
     "Accept": "application/json",
 }
 
 data = {
-    "username": config("AFRICASTALKING_USERNAME"),
+    "username": config("AFRICASTALKING_USERNAME", default='sandbox'),
     "from": config("SMS_SENDER"),
     "message": "Thank you for your order. We will reach out to you shortly.",
     "to": "+254729640480",
