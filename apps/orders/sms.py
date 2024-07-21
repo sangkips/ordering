e4468ff6de97f479e5b7df3442e37d018c48dc22
sangkips <sangkipkoech@gmail.com>
2024-07-21 20:55:01 +0300

    fix: revert failing changes

--- apps/orders/sms.py
+++ apps/orders/sms.py
@@ -4,14 +4,14 @@ from decouple import config
 url = "https://api.sandbox.africastalking.com/version1/messaging"
 
 headers = {
-    "ApiKey": config("AFRICASTALKING_API_KEY"),
+    "ApiKey": config("AFRICASTALKING_API_KEY", default=""),
     "Content-Type": "application/x-www-form-urlencoded",
     "Accept": "application/json",
 }
 
 data = {
-    "username": config("AFRICASTALKING_USERNAME"),
-    "from": config("SMS_SENDER"),
+    "username": config("AFRICASTALKING_USERNAME", default=""),
+    "from": config("SMS_SENDER", default=""),
     "message": "Thank you for your order. We will reach out to you shortly.",
     "to": "+254729640480",
 }
